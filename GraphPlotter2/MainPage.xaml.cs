cc87e2fe1e0498d73cf3a96281a551bc38b6b449
CramelIffy <uriuriezifu@outlook.com>
2023-09-27 23:27:42 +0900

    バージョンを変更

--- GraphPlotter2/MainPage.xaml.cs
+++ GraphPlotter2/MainPage.xaml.cs
@@ -283,7 +283,7 @@ namespace GraphPlotter2
         }
         private void OpenAbout(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("GraphPlotter V2.0.3\nMade by Fujie Riu\nTokushimaUniv. Rocket Project", "バージョン情報", MessageBoxButton.OK);
+            MessageBox.Show("GraphPlotter V2.0.4\nMade by Fujie Riu\nTokushimaUniv. Rocket Project", "バージョン情報", MessageBoxButton.OK);
         }
     }
 }
